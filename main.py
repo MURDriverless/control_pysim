74fbacaf81c119bfe7319a42581868acb99e2783
Dennis Wirya <dwirya@outlook.com>
2020-07-16 01:28:57 +1000

    Try plotting with animation

---
+++ main.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+
+
+# Test plotting with animation
+def animated_plot():
+    # Generate points
+    xs = [i for i in range(-5, 6)]
+    ys = [x**2 for x in xs]
+    dt = 0.2
+
+    # Start plotting
+    plt.cla()
+    # For stopping simulation with the esc key.
+    plt.gcf().canvas.mpl_connect('key_release_event', lambda event: [exit(0) if event.key == 'escape' else None])
+
+    # Plot data points
+    for i in range(len(xs)):
+        plt.plot(xs[i], ys[i], "bx-")
+        plt.xlabel("x[m]")
+        plt.ylabel("y[m]")
+        # Prevent axes from expanding by setting limits. They must be defined here,
+        # if it is applied outside, only the last point gets applied.
+        plt.xlim(-5, 5)
+        plt.ylim(0, 25)
+        plt.pause(dt)
+
+    # Finally render the plot
+    plt.show()
+
+
+if __name__ == "__main__":
+    animated_plot()
